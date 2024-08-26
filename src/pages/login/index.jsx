2af82f15c6f9177ad954da07a54105fa0a44e0c5
vuvansy <vuvansy3001@gmail.com>
2024-08-26 10:56:25 +0700

    Design Login Form

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,4 +1,76 @@
+import { Button, Divider, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import "./login.scss";
+
 const LoginPage = () => {
-    return <>Login page</>;
+    const onFinish = async (values) => {
+        console.log(values);
+    };
+    return (
+        <div className="login-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">Đăng Nhập</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="basic"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item
+                                labelCol={{ span: 24 }}
+                                label="Email"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Email không được để trống!",
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                labelCol={{ span: 24 }}
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message:
+                                            "Mật khẩu không được để trống!",
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Đăng nhập
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal">
+                                Chưa có tài khoản ?
+                                <span>
+                                    <Link to="/register"> Đăng Ký </Link>
+                                </span>
+                            </p>
+                            <br />
+                            <p className="text" style={{ color: "#9d9d9d" }}>
+                                p/s: Để test, sử dụng tài khoản
+                                guest@gmail.com/123456
+                            </p>
+                        </Form>
+                    </section>
+                </div>
+            </main>
+        </div>
+    );
 };
 export default LoginPage;
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,14 @@
-import axios from '../utils/axios-customize';
+import axios from "../utils/axios-customize";
 
 export const callRegister = (fullName, email, password, phone) => {
-    return axios.post('/api/v1/user/register', { fullName, email, password, phone })
-}
+    return axios.post("/api/v1/user/register", {
+        fullName,
+        email,
+        password,
+        phone,
+    });
+};
+
+export const callLogin = (username, password) => {
+    return axios.post("/api/v1/auth/login", { username, password });
+};
