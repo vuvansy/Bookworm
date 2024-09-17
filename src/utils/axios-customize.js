427e8cdd2488d620878af7de6a187c8f07052cee
vuvansy <vuvansy3001@gmail.com>
2024-09-17 20:55:07 +0700

    Axios Retry - Handle Refresh Token

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -7,7 +7,15 @@ const instance = axios.create({
     withCredentials: true, //Tham số để set cookies
 });
 
-instance.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem('access_token')}` }
+instance.defaults.headers.common = {
+    Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+};
+
+const handleRefreshToken = async () => {
+    const res = await instance.get("/api/v1/auth/refresh");
+    if (res && res.data) return res.data.access_token;
+    else null;
+};
 
 // Add a request interceptor
 instance.interceptors.request.use(
@@ -21,6 +29,8 @@ instance.interceptors.request.use(
     }
 );
 
+const NO_RETRY_HEADER = "x-no-retry"; //Fix 401 lặp vô hạn
+
 // Add a response interceptor
 instance.interceptors.response.use(
     function (response) {
@@ -29,9 +39,25 @@ instance.interceptors.response.use(
         // Do something with response data
         return response && response.data ? response.data : response;
     },
-    function (error) {
+    async function (error) {
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
+        if (
+            error.config &&
+            error.response &&
+            +error.response.status === 401 &&
+            !error.config.headers[NO_RETRY_HEADER] //Nếu không có
+        ) {
+            const access_token = await handleRefreshToken();
+            error.config.headers[NO_RETRY_HEADER] = "true"; //Fix 401 lặp vô hạn
+            if (access_token) {
+                error.config.headers[
+                    "Authorization"
+                ] = `Bearer ${access_token}`;
+                localStorage.setItem("access_token", access_token);
+                return instance.request(error.config);
+            }
+        }
         return error?.response?.data ?? Promise.reject(error);
     }
 );
